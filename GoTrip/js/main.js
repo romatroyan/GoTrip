3e47244cdaaa9d5af7bac21ab4b687b5dfb6c93e
Roman <romatroyann@gmail.com>
2023-01-30 12:52:54 +0300

    no mobile

--- GoTrip/js/main.js
+++ GoTrip/js/main.js
@@ -1,17 +1,8 @@
-const owl = $('.owl-carousel');
-owl.owlCarousel({
-	center: true,
-	items: 3,
-	margin: 30, 
-	startPosition: 1,
-	loop: true,
-});
-
-
-$('.slider__button-next').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-$('.slider__button-prev').click(function() {
-
-    owl.trigger('prev.owl.carousel');
-})
+$(document).ready(function(){
+	$('.slick-carusel').slick({
+		centerMode: true,
+		centerPadding: '170px',
+		slidesToShow:2,
+		infinite: true,
+	});
+ });
